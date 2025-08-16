d0a2d58a907c051ed412d96b1f7fb83030863891
Puneet Bhat <152762210+bluetr981@users.noreply.github.com>
2025-08-15 23:23:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,45 +19,5 @@ replacement_rules_feature = {
 def displayInformation():
     return "<h1>The bulk of this website is for the API access of the ACL Injury Website</h1>"
     
-'''@app.route("/healthz", methods=["POST"])
-def inference():
-    features = request.get_json(force=True)
-    input_dict = json.load(features)
-
-    SelectedModel = input_dict["selected_model"]
-    CTS = input_dict["CoronalTibialSlope"]
-    MTS = input_dict["MedialTibialSlope"]
-    LTS = input_dict["LateralTibialSlope"]
-    MTD = input_dict["MedialTibialDepth"]
-    Sex = input_dict["selectsex"]
-
-    input_list = [CTS, MTS, LTS, MTD, Sex]
-
-    inference = {"Prediction: ":perform_inference(SelectedModel, input_list)}
-    
-    return jsonify(inference)
-
-    
-def perform_inference(model_path:str, array:np.array) -> np.array:
-    match = re.search(r'\[([\d,\s]+)\]', model_path)
-    indices_str = match.group(1)
-    indices = [int(idx.strip()) -1 for idx in indices_str.split(',')]
-    input = input[:, indices]
-    means = np.array([2.8280, 5.8495, 7.1613, 2.4280, 0.4194]).reshape(1, -1)[:, indices]
-    stds = np.array([2.0196, 3.2234, 3.0335, 1.0566, 0.4961]).reshape(1, -1)[:, indices]
-    input = (input - means) / stds
-
-    if model_path.endswith('joblib'):
-        model = joblib.load(model_path)
-    elif model_path.endswith('.json'):
-        model = XGBClassifier()
-        model.load_model(model_path)
-    
-    prediction = model.predict(input)
-    return prediction
-    
-    prediction = model.predict(array)
-    return int(prediction[-1])'''
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
