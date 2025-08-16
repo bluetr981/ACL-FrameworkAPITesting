6a6fc72b83c2b82295710e8796d3c3590eafe9fa
Puneet Bhat <152762210+bluetr981@users.noreply.github.com>
2025-08-16 00:58:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def perform_inference(model_path:str, input:np.array) -> np.array:
     match = re.search(r'\[([\d,\s]+)\]', model_path)
     indices_str = match.group(1)
     indices = [int(idx.strip()) -1 for idx in indices_str.split(',')]
-    input = input[:, indices]
+    input = input.reshape(1, -1)[:, indices]
     means = np.array([2.8280, 5.8495, 7.1613, 2.4280, 0.4194]).reshape(1, -1)[:, indices]
     stds = np.array([2.0196, 3.2234, 3.0335, 1.0566, 0.4961]).reshape(1, -1)[:, indices]
     input = (input - means) / stds
