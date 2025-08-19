f84d85216bbd57b4ca60330bdbd984fbed649b85
Puneet Bhat <152762210+bluetr981@users.noreply.github.com>
2025-08-18 21:55:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def displayInformation():
 def inference():
     if (request.method != "GET"):
         data = request.get_json(force=True)
-        return data.get("CoronalTibialSlope")
+        return jsonify({"CTS":data.get("CoronalTibialSlope")})
         
         '''features = request.json()
     
